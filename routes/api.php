257140f886571080c527e5f9898035c4c81b4434
Vera Compagner <s1099891@student.hsleiden.nl>
2021-07-05 14:14:17 +0200

    auth, create, delete gemaakt

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\AuthController;
+
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,6 +16,30 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+
+
+
+//openbare/public routes
+Route::get('/products/search/{name}', [ProductController::class, 'search']);
+Route::get('/products/{id}', [ProductController::class, 'show']);
+Route::get('/products', [ProductController::class, 'index']);
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+
+//protected routes
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    //Alles wat hier in zit is protected
+    // het is dan niet openbaar je moet eerst inloggen (VEILIG DUS)
+    Route::post('/products', [ProductController::class, 'store']);
+    Route::put('/products{id}', [ProductController::class, 'update']);
+    Route::delete('/products/{id}', [ProductController::class, 'destroy']);
+    Route::post('/logout',[AuthController::class, 'logout']);
+
+
+});
+
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
